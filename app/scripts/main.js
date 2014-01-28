daf54ace4023d4885182a2c23288f1086265d365
megrez <lujiajing1126@gmail.com>
2014-01-28 21:44:50 +0800

    fix the Issue #1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,12 +2,14 @@ $(function(){
 	'use strict';
 	$('#loading').hide('slow');
 	$('#wrap').html(template.render('index'));
+	Holder.run();
 	// Bind an event handler.
 });
 $(window).bind('hashchange', function(e) {
 	var hash = location.hash;
 	var template_name = hash.slice(1);
 	$('#wrap').html(template.render(template_name));
+	Holder.run();
 });
 // Manually trigger the event handler.
 $(window).trigger('hashchange');
