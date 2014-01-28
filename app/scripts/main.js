c28d779b1d24e767ff75fe8c3cf2158577cd212f
megrez <lujiajing1126@gmail.com>
2014-01-28 19:08:37 +0800

    add hash check for improvement

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,13 @@
 $(function(){
 	'use strict';
-	$("#loading").hide();
-	$("body").prepend(template.render('index'));
+	$('#loading').hide('slow');
+	$('#wrap').html(template.render('index'));
+	// Bind an event handler.
 });
+$(window).bind('hashchange', function(e) {
+	var hash = location.hash;
+	var template_name = hash.slice(1);
+	$('#wrap').html(template.render(template_name));
+});
+// Manually trigger the event handler.
+$(window).trigger('hashchange');
